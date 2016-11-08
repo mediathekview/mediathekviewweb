de6e879d336388d57a5c63badde6d1cfc377e529
bagbag <bagbag98@googlemail.com>
2016-11-08 22:51:52 +0100

    backup

--- MediathekIndexer.js
+++ MediathekIndexer.js
@@ -1,9 +1,75 @@
-var cp = require('child_process');
+const cp = require('child_process');
+const os = require('os');
+const REDIS = require('redis');
+
+const cpuCount = os.cpus().length;
 
 class MediathekIndexer {
-    constructor(host = '127.0.0.1', port = 6379, passwort = null, flush = true) {
-        
+    constructor(host = '127.0.0.1', port = 6379, password = null, flush = true) {
+        this.workers = [];
+        this.options = {
+            host: host,
+            port: port,
+            password: password,
+            flush: flush
+        };
     }
 
+    indexFile(file) {
+        if (this.options.flush) {
+            let redis = REDIS.createClient({
+                host: host,
+                port: port,
+                password: password
+            });
+            redis.on('ready', () => {
+                redis.flushall((err, reply) => {
+                    console.log('flushed: ' + reply);
+                    redis.quit();
+                    this.startWorkers(file);
+                });
+            });
+        } else {
+            this.startWorkers(file);
+        }
+    }
 
+    startWorkers(file) {
+        for (let i = 0; i < cpuCount; i++) {
+            this.workers.push(this.startWorker(file, i, i));
+        }
+    }
+
+    startWorker(file, skip, offset) {
+        let worker = cp.fork('./MediathekIndexerWorker.js');
+
+        worker.on('message', (message) => {
+            if (message.type == 'notification') {
+                if (message.body == 'ready') {
+                    this.sendMessage(worker, 'command', {
+                        command: 'init',
+                        host: this.options.host,
+                        port: this.options.port,
+                        password: this.options.password
+                    });
+                } else if (message.body == 'initialized') {
+                    this.sendMessage(worker, 'command', {
+                        command: 'indexFile',
+                        file: file,
+                        skip: skip,
+                        offset: offset
+                    });
+                }
+            }
+        });
+
+        return worker;
+    }
+
+    sendMessage(worker, type, body) {
+        worker.send({
+            type: type,
+            body: body
+        });
+    }
 }
--- MediathekIndexerWorker.js
+++ MediathekIndexerWorker.js
@@ -1,48 +1,154 @@
-var REDIS = require('redis');
-var underscore = require('underscore');
+const REDIS = require('redis');
+const lineReader = require('line-reader');
+const underscore = require('underscore');
+
+var indexBuffer = []; //for searchIndex
+var entryBuffer = []; //for indexData
+var entryCounter = 0;
 
 process.on('message', (message) => {
     if (message.type == 'command') {
         if (message.body.command == 'init') {
-            init(message.body.options);
+            init(message.body.host, message.body.port, message.body.password);
+        } else if (message.body.command == 'indexFile') {
+            indexFile(message.body.file, message.body.skip, message.body.offset, message.body.minWordSize);
+        } else {
+            console.log('unrecognized command: ' + message.body.command);
         }
+    } else {
+        console.log('unrecognized message.type: ' + message.type);
     }
 });
 
-function init(options) {
+function init(host, port, password) {
     var searchIndex = REDIS.createClient({
-        host: options.host,
-        port: options.port,
-        password: options.password,
+        host: host,
+        port: port,
+        password: password,
         db: 0
     });
 
     var indexData = REDIS.createClient({
-        host: options.host,
-        port: options.port,
-        password: options.password,
+        host: host,
+        port: port,
+        password: password,
         db: 1
     });
 
-    searchIndex.on("error", (err) => {
+    var searchIndex.on('error', (err) => {
         sendMessage('notification', {
             notification: 'error',
             error: err
         });
     });
 
-    indexData.on("error", (err) => {
+    var indexData.on('error', (err) => {
         sendMessage('notification', {
             notification: 'error',
             error: err
         });
     });
 
-    if (options.flush) {
-        this.searchIndex.flushdb();
-        this.indexData.flushdb();
-        sendMessage
+    var searchIndex.on('ready', () => emitInitialized());
+    var indexData.on('ready', () => emitInitialized());
+}
+
+var initCounter = 0;
+
+function emitInitialized() {
+    if (++initCounter == 2) {
+        sendMessage('notification', {
+            notification: 'initialized'
+        });
     }
+}
+
+
+
+/*"X" : [ "Sender", "Thema", "Titel", "Datum", "Zeit", "Dauer", "Größe [MB]", "Beschreibung", "Url", "Website", "Url Untertitel", "Url RTMP", "Url Klein", "Url RTMP Klein", "Url HD", "Url RTMP HD", "DatumL", "Url History", "Geo", "neu" ] */
+const indexRegex = /"X" : \[ "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)", "(.*?)" ]/;
+
+function indexFile(file, skip, offset, minWordSize) {
+    lineReader.eachLine(file, (line, last, getNext) => {
+
+        var match = indexRegex.exec(line);
+        if (match != null) {
+            var entry = {
+                //channel: match[1],
+                //topic: match[2],
+                title: match[3],
+                //date: match[4],
+                //time: match[5],
+                timestamp: moment(match[4] + match[5], 'DD.MM.YYYYHHmm').unix(),
+                duration: match[6],
+                size: match[7] * 1000000, //MB to bytes
+                //description: match[8],
+                url_video: match[9],
+                url_website: match[10]
+                    /*urls: {
+                        video: match[9],
+                        website: match[10],
+                        //subtitle: match[11],
+                        //rtmp: match[12],
+                        //video_short: match[13],
+                        //rtmp_short: match[14],
+                        //hd: match[15],
+                        //rtmp_hd: match[16],
+                        //history: match[18],
+                    },*/
+                    //dateL: match[17],
+                    //geo: match[19],
+                    //new: match[20]
+            };
+        }
+
+        if (last) {
+            flushIndexBuffer();
+            flushEntryBuffer();
+        }
+    });
+}
+
+function processEntry(entry, minWordSize, getNext) {
+    entryCounter++;
+
+    let splits = entry.title.trim().replace(':', '').toLowerCase().split(' ');
+
+    for (let i in splits) {
+        let split = splits[i];
+
+        for (let begin = 0; begin <= split.length - MIN_WORD_SIZE; begin++) {
+            for (let end = begin + MIN_WORD_SIZE; end <= split.length; end++) {
+                let key = split.slice(begin, end);
+
+                indexBuffer.push(['sadd', key, entryCounter]);
+            }
+        }
+    }
+
+    entryBuffer.push(['hmset', entryCounter, 'title', entry.title, 'timestamp', entry.timestamp, 'duration', entry.duration, 'size', entry.size, 'url_video', entry.url_video, 'url_website', entry.url_website]);
+
+    if (indexBuffer.length >= 500) {
+        flushIndexBuffer();
+    }
+    if (entryBuffer.length >= 200) {
+        flushEntryBuffer();
+    }
+
+    //if (c % 1000 == 0) console.log('indexed ' + c + ' entries, containing ' + this.searchindex.size() + ' mapkeys');
+    redis.dbsize((err, reply) => {
+        if (c % 1000 == 0) console.log('indexed ' + c + ' entries, containing ' + reply + ' mapkeys');
+    });
+}
+
+function flushIndexBuffer() {
+    indexData.batch(indexBuffer).exec();
+    indexBuffer = [];
+}
+
+function flushEntryBuffer() {
+    searchIndex.batch(entryBuffer).exec();
+    entryBuffer = [];
 }
 
 function sendMessage(type, body) {
