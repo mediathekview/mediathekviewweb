b91e9ec2534c4c146bf4eaaee97a3d75a81c505f
bagbag <bagbag98@googlemail.com>
2016-11-18 10:28:38 +0100

    backup

--- app.js
+++ app.js
@@ -20,11 +20,104 @@ var websiteNames = [];
 
 mediathekIndexer.indexFile('../fulldata', config.min_word_size);
 
+var serverStarted = false;
 var workerStates = [];
+
+function startServer() {
+    app.use('/static', express.static('static'));
+
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname + '/index.html'));
+    });
+
+    io.on('connection', (socket) => {
+        socket.on('queryEntry', (query) => {
+            queryEntries(query.queryString, query.mode, query.filters, (result) => {
+                socket.emit('queryResult', result);
+            });
+        });
+        socket.on('getWebsiteNames', () => {
+            searchEngine.getWebsiteNames((result) => {
+                socket.emit('websiteNames', result);
+                websiteNames = result;
+            });
+        });
+    });
+
+    httpServer.listen(8080, () => {
+        console.log('server listening on *:8080');
+    });
+
+    function queryEntries(query, mode, filters, callback) {
+        console.log('querying ' + query);
+        let begin = Date.now();
+
+        searchEngine.search(query, config.min_word_size, mode, (result, err) => {
+            if (err) {
+                console.log(err);
+                callback([]);
+                return;
+            }
+
+            let searchEngineTime = Date.now() - begin;
+            begin = Date.now();
+            let resultCount = result.length;
+
+            if (filters.websiteNames.length != websiteNames.length) {
+                result = result.filter((entry) => {
+                    return filters.websiteNames.includes(entry.data.websiteName);
+                });
+            }
+
+            result = result.sort((a, b) => {
+                let relevanceDiff = b.relevance - a.relevance;
+                if (relevanceDiff == 0) {
+                    let aMoment = moment.unix(a.data.timestamp);
+                    let bMoment = moment.unix(b.data.timestamp);
+
+                    if (aMoment.isSameOrAfter(bMoment))
+                        return -1;
+                    else
+                        return 1;
+                } else {
+                    return relevanceDiff;
+                }
+            }).slice(0, 50);
+
+            let filterTime = Date.now() - begin;
+            let queryInfo = {
+                searchEngineTime: searchEngineTime,
+                filterTime: filterTime,
+                resultCount: resultCount
+            };
+
+            callback({
+                data: result,
+                queryInfo: queryInfo
+            });
+
+            console.log('query took ' + (Date.now() - begin) / 1000 + ' seconds');
+
+        });
+    }
+}
 
 mediathekIndexer.on('workerState', (workerIndex, state) => {
     workerStates[workerIndex] = state;
     logWorkerStates();
+
+    for (var i = 0; i < workerStates.length; i++) {
+        if (workerStates[i] == undefined)
+            return;
+
+        if (!workerStates[i].done)
+            return;
+    }
+
+    if (!serverStarted) {
+        serverStarted = true;
+        startServer();
+    }
 });
 
 function logWorkerStates() {
@@ -41,81 +134,4 @@ function logWorkerStates() {
         console.log('\tindices: ' + state.indices);
         console.log('\ttime: ' + (state.time / 1000) + ' seconds');
     }
-}
-
-app.use('/static', express.static('static'));
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-io.on('connection', (socket) => {
-    socket.on('queryEntry', (query) => {
-        queryEntries(query.queryString, query.mode, query.filters, (result) => {
-            socket.emit('queryResult', result);
-        });
-    });
-    socket.on('getWebsiteNames', () => {
-        searchEngine.getWebsiteNames((result) => {
-            socket.emit('websiteNames', result);
-            websiteNames = result;
-        });
-    });
-});
-
-httpServer.listen(8080, () => {
-    console.log('server listening on *:8080');
-});
-
-function queryEntries(query, mode, filters, callback) {
-    console.log('querying ' + query);
-    let begin = Date.now();
-
-    searchEngine.search(query, config.min_word_size, mode, (result, err) => {
-        if (err) {
-            console.log(err);
-            callback([]);
-            return;
-        }
-
-        let searchEngineTime = Date.now() - begin;
-        begin = Date.now();
-        let resultCount = result.length;
-
-        if (filters.websiteNames.length != websiteNames.length) {
-            result = result.filter((entry) => {
-                return filters.websiteNames.includes(entry.data.websiteName);
-            });
-        }
-
-        result = result.sort((a, b) => {
-            let relevanceDiff = b.relevance - a.relevance;
-            if (relevanceDiff == 0) {
-                let aMoment = moment.unix(a.data.timestamp);
-                let bMoment = moment.unix(b.data.timestamp);
-
-                if (aMoment.isSameOrAfter(bMoment))
-                    return -1;
-                else
-                    return 1;
-            } else {
-                return relevanceDiff;
-            }
-        }).slice(0, 50);
-
-        let filterTime = Date.now() - begin;
-        let queryInfo = {
-            searchEngineTime: searchEngineTime,
-            filterTime: filterTime,
-            resultCount: resultCount
-        };
-
-        callback({
-            data: result,
-            queryInfo: queryInfo
-        });
-
-        console.log('query took ' + (Date.now() - begin) / 1000 + ' seconds');
-
-    });
 }
