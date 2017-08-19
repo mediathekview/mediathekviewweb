63642c4e86ceddee22b0a296a1b38b799db38c11
bagbag <bagbag98@googlemail.com>
2017-08-19 22:43:39 +0200

    chaged redis paramater to only redis-options

--- server/src/queue/provider.ts
+++ server/src/queue/provider.ts
@@ -1,15 +1,12 @@
 import * as Bull from 'bull';
 import * as Redis from 'ioredis';
-import config from '../config';
-
-const REDIS_OPTIONS: Redis.RedisOptions = { host: config.redis.host, port: config.redis.port, db: config.redis.db };
 
 export type ImportQueueType = { ressource: string, timestamp: number };
 
 export class QueueProvider {
-  constructor(private redis: Redis.Redis) { }
+  constructor(private redisOptions: Redis.RedisOptions) { }
 
   getImportQueue(): Bull.Queue {
-    return new Bull('filmlist-import', { prefix: 'queue:', redis: REDIS_OPTIONS });
+    return new Bull('filmlist-import', { prefix: 'queue:', redis: this.redisOptions });
   }
 }
