5a7f9ec4b597eb26ddbe6bad4ac6de3eec3298e6
bagbag <bagbag98@googlemail.com>
2017-08-20 13:52:02 +0200

    changed strategy of importing entries

--- server/src/filmlist-importer/filmlist-importer.ts
+++ server/src/filmlist-importer/filmlist-importer.ts
@@ -1,12 +1,10 @@
-import { IDatastoreProvider, IKey, ISet, ISortedSet, SortedSetMember, IMap, AggregationMode } from '../data-store';
+import { IDatastoreProvider, IKey, ISet, ISortedSet, SortedSetMember, IMap, Aggregation } from '../data-store';
 import { IFilmlist } from './filmlist-interface';
 import { IEntry, IFilmlistMetadata } from '../common';
 import { FilmlistParser } from './filmlist-parser';
-import { IFilmlistProvider } from './filmlist-provider-interface';
 import config from '../config';
 import { random } from '../utils';
 import * as Bull from 'bull';
-import { ILockProvider, ILock } from '../lock';
 import { QueueProvider, ImportQueueType } from '../queue';
 import { HttpFilmlist } from './http-filmlist';
 import { DatastoreKeys } from '../data-store-keys';
@@ -31,56 +29,60 @@ export class FilmlistImporter {
   private async process(job: Bull.Job) {
     const data: ImportQueueType = job.data;
 
+    console.log('importing', data.ressource);
+
     let filmlist: IFilmlist;
     let metadata: IFilmlistMetadata | undefined;
 
     if (data.ressource.startsWith('http')) {
       filmlist = new HttpFilmlist(data.ressource, data.timestamp);
     } else {
-      return Promise.reject(new Error(`no filmlist-handler for ressource ${data.ressource} available`));
+      throw new Error(`no filmlist-handler for ressource ${data.ressource} available`);
     }
 
     const parser = new FilmlistParser(filmlist, (_metadata) => metadata = _metadata);
 
-    const tempEntryIDTimestampSortedSet = this.datastoreProvider.getSortedSet();
-
     let sortedSetBuffer: SortedSetMember<string>[] = [];
     const mapBuffer: Map<string, IEntry> = new Map();
 
+    let counter = 0;
     for await (const entry of parser.parse()) {
       if (metadata == undefined) {
-        return Promise.reject(new Error('no filmlist-metadata emitted, something is wrong'));
+        throw new Error('no filmlist-metadata emitted, something is wrong');
       }
 
       sortedSetBuffer.push({ key: entry.id, score: metadata.timestamp });
       mapBuffer.set(entry.id, entry);
 
       if (sortedSetBuffer.length >= 250) {
-        const transaction = this.datastoreProvider.getTransaction();
-        tempEntryIDTimestampSortedSet.transact(transaction);
-        this.entryMap.transact(transaction);
+        await this.flushBuffer(sortedSetBuffer, mapBuffer, metadata);
+      }
 
-        tempEntryIDTimestampSortedSet.add(...sortedSetBuffer);
-        this.entryMap.set(mapBuffer);
-
-        tempEntryIDTimestampSortedSet.endTransact();
-        this.entryMap.endTransact();
-
-        await transaction.exec();
-
-        sortedSetBuffer = [];
-        mapBuffer.clear();
+      if (++counter % 1000 == 0) {
+        console.log(counter);
       }
     }
+    await this.flushBuffer(sortedSetBuffer, mapBuffer, metadata as IFilmlistMetadata);
 
+    console.log(counter);
+
+    await this.importedFilmlistTimestamps.add(data.timestamp);
+  }
+
+  private async flushBuffer(sortedSetBuffer: SortedSetMember<string>[], mapBuffer: Map<string, IEntry>, metadata: IFilmlistMetadata) {
     const transaction = this.datastoreProvider.getTransaction();
-    tempEntryIDTimestampSortedSet.transact(transaction);
-    tempEntryIDTimestampSortedSet.union(this.entryIDTimestampSortedSet, AggregationMode.Max, this.entryIDTimestampSortedSet);
-    tempEntryIDTimestampSortedSet.delete();
-    tempEntryIDTimestampSortedSet.endTransact();
+    this.entryIDTimestampSortedSet.transact(transaction);
+    this.entryMap.transact(transaction);
+
+    this.entryMap.set(mapBuffer, { set: this.entryIDTimestampSortedSet, lessThan: metadata.timestamp });
+    this.entryIDTimestampSortedSet.add(sortedSetBuffer, Aggregation.Max);
+
+    this.entryIDTimestampSortedSet.endTransact();
+    this.entryMap.endTransact();
 
     await transaction.exec();
 
-    await this.importedFilmlistTimestamps.add(data.timestamp);
+    sortedSetBuffer.length = 0;
+    mapBuffer.clear();
   }
 }
