8f2467db783f2165b12e15953c6fc6b180b361e1
bagbag <bagbag98@googlemail.com>
2017-08-20 21:54:01 +0200

    improved lock

--- server/src/lock/redis/lock.ts
+++ server/src/lock/redis/lock.ts
@@ -1,13 +1,32 @@
 import { ILock } from '../';
+import { sleep, random } from '../../utils';
 import * as Redis from 'ioredis';
 
 export class RedisLock implements ILock {
-  private relockTimeout: NodeJS.Timer;
+  private relockTimeout: NodeJS.Timer | null;
 
   constructor(private key: string, private id: string, private redis: Redis.Redis) { }
 
   async lock(time?: number): Promise<boolean> {
-    const result = await (this.redis as any).lock(this.key, this.id, (time == undefined) ? 1000 : time);
+    this.clearTimeout();
+
+    let result;
+    for (let i = 0; i < 5; i++) {
+      const _time = Math.floor((time == undefined) ? 1000 : time);
+
+      if (_time <= 0) {
+       throw new Error('cannot lock for zero or negative time');
+      }
+
+      result = await (this.redis as any).lock(this.key, this.id, _time);
+
+      if (result == 'OK') {
+        break;
+      }
+
+      const timeLeft = await this.timeLeft(false);
+      await sleep(150 + (timeLeft / 2) + random(-50, 50, true));
+    }
 
     if (result == 'OK') {
       if (time == undefined) {
@@ -21,7 +40,7 @@ export class RedisLock implements ILock {
   }
 
   async unlock(): Promise<boolean> {
-    clearTimeout(this.relockTimeout);
+    this.clearTimeout();
     const result = await (this.redis as any).unlock(this.key, this.id);
     return result == 1;
   }
@@ -29,5 +48,24 @@ export class RedisLock implements ILock {
   async haslock(): Promise<boolean> {
     const result = await (this.redis as any).haslock(this.key, this.id);
     return result == 1;
+  }
+
+  async timeLeft(ownedOnly: boolean): Promise<number> {
+    if (ownedOnly) {
+      const has = await this.haslock();
+
+      if (!has) {
+        return 0;
+      }
+    }
+
+    const result = await (this.redis as any).pttl(this.key);
+
+    return Math.max(result, 0);
+  }
+
+  private clearTimeout() {
+    clearTimeout(this.relockTimeout as NodeJS.Timer);
+    this.relockTimeout = null;
   }
 }
