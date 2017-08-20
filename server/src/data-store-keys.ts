7ac732f27fd260da86ca511ba6d52df3e741403f
bagbag <bagbag98@googlemail.com>
2017-08-20 21:56:12 +0200

    added more keys and queue provider

--- server/src/data-store-keys.ts
+++ server/src/data-store-keys.ts
@@ -2,14 +2,24 @@ interface IDatastoreKeys {
   ImportedFilmlistTimestamps: string;
   EntryMap: string;
   EntryIDTimestampSortedSet: string;
-  LastCheckTimestamp: string;
+  LastFilmlistsCheckTimestamp: string;
+  ImportIDCounter: string;
+  TrackingSetsSortedSet: string;
+  LastIndexedImportID: string;
+  getTrackingSet(id: number): string;
 }
 
 class _DatastoreKeys {
   static EntryMap = 'entryMap';
   static EntryIDTimestampSortedSet = 'entryIDTimestamps';
   static ImportedFilmlistTimestamps = 'importedFilmlistTimestamps';
-  static LastCheckTimestamp = 'lastCheckTimestamp'
+  static LastFilmlistsCheckTimestamp = 'lastFilmlistsCheckTimestamp';
+  static ImportIDCounter = 'importIDCounter';
+  static TrackingSetsSortedSet = 'trackingSets';
+  static LastIndexedImportID = 'lastIndexedImportID';
+  static getTrackingSet(id: number): string {
+    return `track:${id}`;
+  }
 }
 
 export const DatastoreKeys = _DatastoreKeys as IDatastoreKeys;
--- server/src/queue/provider.ts
+++ server/src/queue/provider.ts
@@ -2,11 +2,16 @@ import * as Bull from 'bull';
 import * as Redis from 'ioredis';
 
 export type ImportQueueType = { ressource: string, timestamp: number };
+export type IndexEntriesType = { idsSetKey: string, amount: number };
 
 export class QueueProvider {
   constructor(private redisOptions: Redis.RedisOptions) { }
 
   getImportQueue(): Bull.Queue {
     return new Bull('filmlist-import', { prefix: 'queue', redis: this.redisOptions });
+  }
+
+  getIndexEntriesQueue():Bull.Queue {
+    return new Bull('index-entries', { prefix: 'queue', redis: this.redisOptions });
   }
 }
