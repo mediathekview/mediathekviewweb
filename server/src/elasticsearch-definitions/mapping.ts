35c4a6e508ceaa7ef2cdd48550624cbe5e3865f7
bagbag <bagbag98@googlemail.com>
2017-08-21 22:04:04 +0200

    defined elasticsearch mapping

---
+++ server/src/elasticsearch-definitions/mapping.ts
@@ -0,0 +1,164 @@
+/*
+export interface IEntry {
+  id: string;
+  metadata?: IEntryMetadata;
+
+  channel: string;
+  topic: string;
+  title: string;
+  timestamp: number;
+  duration: number;
+  description: string;
+  website: string;
+  media: IMedia[];
+}
+
+export interface IEntryMetadata {
+  lastSeen: number;
+  downloads: IUserAction[];
+  plays: IUserAction[];
+  secondsPlayed?: number;
+  secondsPaused?: number;
+}
+
+export interface IUserAction {
+  userID: string;
+  timestamp: number;
+}
+
+export interface IMedia {
+  type: MediaType;
+  url: string;
+  size: number;
+}
+*/
+
+const UserActionMapping = {
+  type: 'nested',
+  dynamic: false,
+  include_in_all: false,
+  properties: {
+    userID: {
+      type: 'keyword',
+      index: false,
+      include_in_all: false
+    },
+    timestamp: {
+      type: 'long',
+      index: true,
+      include_in_all: false
+    }
+  }
+}
+
+export const ElasticsearchMapping = {
+  properties: {
+    channel: {
+      type: 'text',
+      index: true,
+      include_in_all: false,
+      analyzer: 'mvw_index_analyzer',
+      search_analyzer: 'mvw_search_analyzer',
+      fielddata: true,
+      fields: {
+        keyword: {
+          type: 'keyword'
+        }
+      }
+    },
+    topic: {
+      type: 'text',
+      index: true,
+      include_in_all: false,
+      analyzer: 'mvw_index_analyzer',
+      search_analyzer: 'mvw_search_analyzer',
+      fielddata: true,
+      fields: {
+        keyword: {
+          type: 'keyword'
+        }
+      }
+    },
+    title: {
+      type: 'text',
+      index: true,
+      include_in_all: false,
+      analyzer: 'mvw_index_analyzer',
+      search_analyzer: 'mvw_search_analyzer'
+    },
+    description: {
+      type: 'text',
+      index: true,
+      include_in_all: false,
+      analyzer: 'mvw_index_analyzer',
+      search_analyzer: 'mvw_search_analyzer'
+    },
+    timestamp: {
+      type: 'date',
+      index: true,
+      include_in_all: false,
+      format: 'epoch_second',
+    },
+    duration: {
+      type: 'long',
+      index: true,
+      include_in_all: false,
+    },
+    website: {
+      type: 'keyword',
+      index: false,
+      include_in_all: false
+    },
+    media: {
+      type: 'nested',
+      dynamic: false,
+      include_in_all: false,
+      properties: {
+        type: {
+          type: 'byte',
+          index: true,
+          include_in_all: false
+        },
+        url: {
+          type: 'keyword',
+          index: false,
+          include_in_all: false
+        },
+        size: {
+          type: 'long',
+          index: true,
+          include_in_all: false
+        },
+        quality: {
+          type: 'byte',
+          index: true,
+          include_in_all: false
+        }
+      }
+    },
+    metadata: {
+      type: 'object',
+      dynamic: false,
+      include_in_all: false,
+      properties: {
+        lastSeen: {
+          type: 'long',
+          index: true,
+          include_in_all: false
+        },
+        downloads: UserActionMapping,
+        plays: UserActionMapping,
+        secondsPlayed: {
+          type: 'long',
+          index: true,
+          include_in_all: false
+        },
+        secondsPaused: {
+          type: 'long',
+          index: true,
+          include_in_all: false
+        }
+      }
+    }
+  }
+}
