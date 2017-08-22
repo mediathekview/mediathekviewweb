fdfec413a9008a7356c0b9fc64ff6b615d3f2389
bagbag <bagbag98@googlemail.com>
2017-08-22 21:31:10 +0200

    added elasticsearch settings definition

--- server/src/elasticsearch-definitions/settings.ts
+++ server/src/elasticsearch-definitions/settings.ts
@@ -1,3 +1,27 @@
 export const ElasticsearchSettings = {
-
+  settings: {
+      analysis: {
+        filter: {
+          german_stop: {
+            type:       'stop',
+            stopwords:  '_german_'
+          },
+          german_stemmer: {
+            type:       'stemmer',
+            language:   'light_german'
+          }
+        },
+        analyzer: {
+          german: {
+            tokenizer:  'standard',
+            filter: [
+              'lowercase',
+              'german_stop',
+              'german_normalization',
+              'german_stemmer'
+            ]
+          }
+        }
+      }
+    }
 }
