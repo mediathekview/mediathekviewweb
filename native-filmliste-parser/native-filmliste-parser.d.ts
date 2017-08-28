9a094034c55befac19f4521d91f41766124e71f0
bagbag <bagbag98@googlemail.com>
2017-08-28 21:51:44 +0200

    removed native-filmliste-parser

--- native-filmliste-parser/native-filmliste-parser.d.ts
+++
@@ -1,8 +0,0 @@
-import { Entry } from '../model';
-
-export type BatchCallbackType = (data: Entry[], next: () => void) => void;
-
-export class NativeFilmlisteParser {
-  static parseFilmliste(file: string, regex: string, batchSize: number, batchCallback: BatchCallbackType, endCallback: () => void): void;
-  static parseFilmliste(file: string, regex: string, batchSize: number, batchCallback: BatchCallbackType): Promise<void>;
-}
