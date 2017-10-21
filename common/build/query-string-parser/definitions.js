53b9fa328c3c91a0df5c54c0b8efbb43f87ba63c
Patrick Hein <bagbag98@googlemail.com>
2017-10-21 23:18:19 +0200

    removed build dir, added build and dist dir to gitignores

--- common/build/query-string-parser/definitions.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REGEX_CHAR = '/';
-exports.QUOTE_CHARS = ['"', exports.REGEX_CHAR];
-exports.ESCAPE_CHAR = '\\';
--- common/build/search-engine/query/builder/match-all.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MatchAllQueryBuilder {
-    build() {
-        return { matchAll: {} };
-    }
-}
-exports.MatchAllQueryBuilder = MatchAllQueryBuilder;
