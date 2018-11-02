2a9e38ce093567b6a79afc0b6880a3653599c2a0
Patrick Hein <bagbag98@googlemail.com>
2018-11-02 22:13:56 +0100

    added args to build script

--- ci/build-image.sh
+++ ci/build-image.sh
@@ -2,10 +2,11 @@
 
 dockerfile=Dockerfile
 
-while getopts "c:n:f:" opt; do
+while getopts "c:n:f:a:" opt; do
     case $opt in
         c) caches+=("$OPTARG");;
         n) names+=("$OPTARG");;
+        a) args+=("$OPTARG");;
         f) dockerfile=("$OPTARG")
         #...
     esac
@@ -13,9 +14,21 @@ done
 
 shift $((OPTIND -1))
 workdir=$1
-build_arguments="--pull -f $dockerfile $( printf " --cache-from %s" "${caches[@]}" )$( printf " --tag %s" "${names[@]}" ) $workdir"
+build_arguments="--pull -f $dockerfile"
 
-echo $build_arguments
+if [ ${#args[@]} -eq 0 ]; then
+  build_arguments+="$( printf " --build-arg %s" "${args[@]}" )"
+fi
+
+if [ ${#caches[@]} -eq 0 ]; then
+  build_arguments+="$( printf " --cache-from %s" "${caches[@]}" )"
+fi
+
+if [ ${#names[@]} -eq 0 ]; then
+  build_arguments+="$( printf " --tag %s" "${names[@]}" )"
+fi
+
+build_arguments+=" $workdir"
 
 for image in "${caches[@]}"; do
   docker pull $image || true
