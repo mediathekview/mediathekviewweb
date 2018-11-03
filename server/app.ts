58f9ef3c004eaab6a57ba1d89c2258a74b75310a
Patrick Hein <bagbag98@googlemail.com>
2018-11-03 22:29:26 +0100

    added another orf cdn to CSP

--- server/app.ts
+++ server/app.ts
@@ -77,7 +77,7 @@ import * as utils from './utils';
 
   app.use((request, response, next) => {
     const webSocketSource = (request.protocol === 'http' ? 'ws://' : 'wss://') + request.get('host');
-    const orfCdn = 'https://apasfiis.sf.apa.at';
+    const orfCdn = 'https://apasfiis.sf.apa.at https://varorfvod.sf.apa.at';
 
     response.set({
       'Content-Security-Policy': `default-src 'none'; script-src 'self'; worker-src 'self' blob:; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self' data:; connect-src 'self' ${webSocketSource} ${orfCdn}; media-src * blob:; base-uri 'none'; form-action 'none'; frame-ancestors 'none';`,
